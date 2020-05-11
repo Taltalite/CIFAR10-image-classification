01405f40d7d573eae3acdde6d2c341639f0a18c4
Taltalite <44615178+Taltalite@users.noreply.github.com>
2020-05-11 23:21:50 +0800

    dataload

---
+++ dataload.py
@@ -0,0 +1,12 @@
+import numpy
+from keras.datasets import cifar10
+import numpy as np
+np.random.seed(10)
+# 导入数据集，如果没有就会自动下载
+(x_img_train,y_label_train),(x_img_test, y_label_test)=cifar10.load_data()
+print('train:',len(x_img_train))
+print('test :',len(x_img_test))
+print('train_image :',x_img_train.shape)
+print('train_label :',y_label_train.shape)
+print('test_image :',x_img_test.shape)
+print('test_label :',y_label_test.shape)
